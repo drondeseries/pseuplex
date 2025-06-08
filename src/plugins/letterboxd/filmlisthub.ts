76cb074a7e45513e5fee85118b204e3d75616536
Luis Finke <luisfinke@gmail.com>
2025-06-08 18:47:48 -0400

    letterboxd film list hub page

---
+++ src/plugins/letterboxd/filmlisthub.ts
@@ -0,0 +1,100 @@
+import letterboxd from 'letterboxd-retriever';
+import {
+	PseuplexFeedHub,
+	PseuplexFeedHubOptions,
+	PseuplexMetadataTransformOptions,
+	PseuplexRequestContext
+} from '../../pseuplex';
+import * as plexTypes from '../../plex/types';
+import * as lbtransform from './transform';
+import { LetterboxdMetadataProvider } from './metadata';
+
+export enum LetterboxdListItemTokenSource {
+	ID = 'id',
+	FilmID = 'film.id',
+}
+
+export type LetterboxdFilmListHubOptions = PseuplexFeedHubOptions & {
+	letterboxdMetadataProvider: LetterboxdMetadataProvider;
+	metadataTransformOptions: PseuplexMetadataTransformOptions;
+	itemTokenSource?: LetterboxdListItemTokenSource | undefined | null;
+};
+
+export type LetterboxdFilmListPageFetcher = (pageToken: string | null) => Promise<letterboxd.FilmListPage>;
+
+export class LetterboxdFilmListHub extends PseuplexFeedHub<letterboxd.Film,number,string,LetterboxdFilmListHubOptions> {
+	_fetchPage: LetterboxdFilmListPageFetcher;
+
+	constructor(options: LetterboxdFilmListHubOptions, fetchPage: LetterboxdFilmListPageFetcher) {
+		super(options);
+		this._fetchPage = fetchPage;
+	}
+
+	get metadataTransformOptions(): PseuplexMetadataTransformOptions {
+		return this._options.metadataTransformOptions;
+	}
+
+	override parseItemTokenParam(itemToken: string | number): number | undefined {
+		if(this._options.itemTokenSource) {
+			// parse string as number
+			if(typeof itemToken != 'string') {
+				return itemToken;
+			}
+			const parsedItemToken = Number.parseInt(itemToken);
+			if(Number.isNaN(parsedItemToken)) {
+				return itemToken as any;
+			}
+			return parsedItemToken;
+		}
+		return undefined;
+	}
+
+	override compareItemTokens(itemToken1: number, itemToken2: number) {
+		if(this._options.itemTokenSource) {
+			if(itemToken1 == null || itemToken2 == null) {
+				// if one of the items has a null token, we can't compare tokens
+				//  so we will always assume "reloads" of the list come before the old version
+				return -1;
+			}
+			// larger token is first (ie, newer date)
+			return itemToken2 - itemToken1;
+		} else {
+			// Since we aren't using tokens and only load this list once,
+			//  we will always assume "reloads" of the list come before the old version
+			return -1;
+		}
+	}
+
+	override async fetchPage(pageToken: string | null) {
+		const page = await this._fetchPage(pageToken);
+		return {
+			items: page.items.map((listItem) => {
+				let token: any = undefined;
+				switch(this._options.itemTokenSource) {
+					case LetterboxdListItemTokenSource.ID: {
+						// parse string as number
+						token = listItem.id;
+						if(token != null && typeof token === 'string') {
+							const parsedToken = Number.parseInt(token);
+							if(!Number.isNaN(parsedToken)) {
+								token = parsedToken;
+							}
+						}
+					} break;
+				}
+				return {
+					id: listItem.id,
+					token: token,
+					item: listItem.film
+				};
+			}),
+			hasMore: (page.nextPageHref ? true : false),
+			totalItemCount: page.items?.length ?? 0,
+			nextPageToken: page.nextPageHref,
+		};
+	}
+
+	override async transformItem(item: letterboxd.Film, context: PseuplexRequestContext): Promise<plexTypes.PlexMetadataItem> {
+		return await lbtransform.transformLetterboxdFilmHubEntry(item, context, this._options.letterboxdMetadataProvider, this.metadataTransformOptions);
+	}
+}
