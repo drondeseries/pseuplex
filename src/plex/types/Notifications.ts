5b0aed3995a267a3443e839ac37ba05656a8260a
Luis Finke <luisfinke@gmail.com>
2025-07-27 19:04:12 -0400

    define more notification types + container types

--- src/plex/types/Notifications.ts
+++ src/plex/types/Notifications.ts
@@ -1,27 +1,28 @@
+import { PlexMediaItemTypeNumeric, PlexPluginIdentifier } from "./common";
 
 export type PlexNotificationMessage = {
-	NotificationContainer: PlexNotification
+	NotificationContainer: PlexNotificationContainer
 };
 
 export enum PlexNotificationType {
 	Activity = 'activity',
+	Progress = 'progress',
+	Status = 'status',
+	BackgroundProcessingQueue = 'backgroundProcessingQueue',
+	Timeline = 'timeline',
 	Playing = 'playing',
-	UpdateStateChange = 'update.statechange'
+	UpdateStateChange = 'update.statechange',
 };
 
-export type PlexNotification = {
-	type: PlexNotificationType.Activity;
-	size: number;
-	ActivityNotification: PlexActivityNotification[];
-} | {
-	type: PlexNotificationType.Playing;
-	size: number;
-	PlaySessionStateNotification: PlexPlaySessionStateNotification[];
-} | {
-	type: PlexNotificationType.UpdateStateChange;
-	size: number;
-	AutoUpdateNotification: PlexAutoUpdateNotification[];
-};
+export type PlexNotificationContainer =
+	PlexActivityNotificationContainer
+	| PlexProgressNotificationContainer
+	| PlexStatusNotificationContainer
+	| PlexBackgroundProcessingQueueEventNotificationContainer
+	| PlexTimelineEntryNotificationContainer
+	| PlexPlaySessionStateNotificationContainer
+	| PlexAutoUpdateNotificationContainer;
+
 
 
 // Activity Event
@@ -33,7 +34,11 @@ export enum PlexActivityEventType {
 };
 
 export enum PlexActivityType {
-	LibraryRefreshItems = 'library.refresh.items'
+	LibraryRefreshItems = 'library.refresh.items',
+	LibraryUpdateSection = 'library.update.section',
+	LibraryUpdateItemMetadata = 'library.update.item.metadata',
+	ProviderSubscriptionsProcess = 'provider.subscriptions.process',
+	MediaGenerateCredits = 'media.generate.credits',
 }
 
 export type PlexActivityNotification = {
@@ -46,16 +51,116 @@ export type PlexActivityNotification = {
 		userID: number;
 		title: string;
 		subtitle: string;
-		progress: number;
+		progress: number; // 0, 100
 		Context?: {
-			key: string;
+			key?: string;
 			accessible?: boolean;
 			exists?: boolean;
 			refreshed?: boolean;
 			analyzed?: boolean;
+			librarySectionID?: `${number}` | string; // "1"
 		}
 	}
 };
+
+export type PlexActivityNotificationContainer = {
+	type: PlexNotificationType.Activity;
+	size: number;
+	ActivityNotification: PlexActivityNotification[];
+};
+
+
+
+// Progress Event
+
+export type PlexProgressNotification = {
+	message: string; // "Scanning Legend (1985) US (1080p BluRay x265 HEVC 10bit AAC 5.1)"
+};
+
+export type PlexProgressNotificationContainer = {
+	type: PlexNotificationType.Progress;
+	size: number;
+	ProgressNotification: PlexProgressNotification[];
+};
+
+
+
+// Status Event
+
+export enum PlexStatusNotificationName {
+	LibraryUpdate = 'LIBRARY_UPDATE'
+}
+
+export type PlexStatusNotification = {
+	title: string; // "Scanning the \"Movies\" section"
+	description: string;
+	notificationName: PlexStatusNotificationName;
+};
+
+export type PlexStatusNotificationContainer = {
+	NotificationContainer: {
+		type: PlexNotificationType.Status,
+		size: number,
+		StatusNotification: PlexStatusNotification[]
+	}
+};
+
+
+
+// Background Processing
+
+export enum PlexBackgroundProcessingQueueEventType {
+	QueueRegenerated = 'queueRegenerated',
+}
+
+export type PlexBackgroundProcessingQueueEventNotification = {
+	queueID: number; // 1
+	event: PlexBackgroundProcessingQueueEventType;
+};
+
+export type PlexBackgroundProcessingQueueEventNotificationContainer = {
+	type: PlexNotificationType.BackgroundProcessingQueue;
+	size: number;
+	BackgroundProcessingQueueEventNotification: PlexBackgroundProcessingQueueEventNotification[];
+};
+
+
+
+// Timeline Entry
+
+export enum PlexTimelineEntryNotificationState {
+	Added = 0,
+	State1 = 1,
+	State2 = 2,
+	State3 = 3,
+	AddingExtras = 4,
+	FinishedAdding = 5,
+};
+
+export enum PlexTimelineEntryNotificationMetadataState {
+	Created = 'created',
+	Processing = 'processing',
+	Loading = 'loading',
+	Queued = 'queued',
+}
+
+export type PlexTimelineEntryNotification = {
+	identifier: PlexPluginIdentifier;
+	sectionID: `${number}` | string; // "1", "-1"
+	itemID: `${number}` | string; // "45"
+	type: PlexMediaItemTypeNumeric;
+	title: string;
+	state: PlexTimelineEntryNotificationState; // 0,
+	metadataState?: PlexTimelineEntryNotificationMetadataState; // "created",
+	updatedAt: number; // 1753656127
+};
+
+export type PlexTimelineEntryNotificationContainer = {
+	type: PlexNotificationType.Timeline;
+	size: number;
+	TimelineEntry: PlexTimelineEntryNotification[];
+};
+
 
 
 // Play Session State Event
@@ -76,7 +181,14 @@ export type PlexPlaySessionStateNotification = {
 	playQueueItemID: number;
 	playQueueID: number;
 	state: PlexPlaySessionState;
-}
+};
+
+export type PlexPlaySessionStateNotificationContainer = {
+	type: PlexNotificationType.Playing;
+	size: number;
+	PlaySessionStateNotification: PlexPlaySessionStateNotification[];
+};
+
 
 
 // Update State Change
@@ -85,4 +197,10 @@ export type PlexAutoUpdateNotification = {
 	key: string;
 	version: string;
 	state: 'done';
+};
+
+export type PlexAutoUpdateNotificationContainer = {
+	type: PlexNotificationType.UpdateStateChange;
+	size: number;
+	AutoUpdateNotification: PlexAutoUpdateNotification[];
 };
